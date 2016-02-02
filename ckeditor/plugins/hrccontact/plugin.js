e9ad6dec4939c25a4198fe7332ce7bc751cfaddb
john gregory <john.gregory@mediagg.com>
2016-02-02 08:18:08 -0500

    Rename of contact.png to hrccontacts.png
    This was needed to make the icon show properly in the toolbar.
    Apparently, the icon .png file must be named the same as the plugin in
    order for CKEditor to render the image in the toolbar

--- ckeditor/plugins/hrccontact/plugin.js
+++ ckeditor/plugins/hrccontact/plugin.js
@@ -1,6 +1,6 @@
 CKEDITOR.plugins.add( 'hrccontact', {
 	// Register the icons.
-	icons: 'contacts',
+	icons: 'hrccontact',
 	// The plugin initialization logic goes inside this method.
 	init: function( editor ) {
 		// Define an editor command that opens our dialog window.
@@ -16,7 +16,7 @@ CKEDITOR.plugins.add( 'hrccontact', {
 			editor.addMenuGroup( 'hrccontactGroup' );
 			editor.addMenuItem( 'hrccontactItem', {
 				label: 'Edit Contact',
-				icon: this.path + 'icons/contacts.png',
+				icon: this.path + 'icons/hrccontact.png',
 				command: 'hrccontact',
 				group: 'hrccontactGroup'
 			});
@@ -29,5 +29,19 @@ CKEDITOR.plugins.add( 'hrccontact', {
 
 		// Register our dialog file -- this.path is the plugin folder path.
 		CKEDITOR.dialog.add( 'contactDialog', this.path + 'dialogs/contactDialog.js' );
+
+        if(typeof editor.config.contentsCss == 'object') {
+        	editor.config.contentsCss.push(CKEDITOR.getUrl(this.path + 'contacts.css'));
+        } else {
+        	editor.config.contentsCss = [editor.config.contentsCss, CKEDITOR.getUrl(this.path + 'contacts.css')];
+        }
 	}
 });
+
+function addRow() {
+    $("#contactsTable").append("<tr><td contenteditable></td><td contenteditable></td><td contenteditable></td><td contenteditable></td></tr>");
+}
+
+function resetDialog () {
+    $(".contactsHolder").html('<table id="contactsTable" width="100%"><tr><th>Name</th><th>Title</th><th>Email</th><th>Phone</th></tr><tr><td contenteditable></td><td contenteditable></td><td contenteditable></td><td contenteditable></td></tr></table><input type="button" class="addRowBtn" value="Add Row" onclick="addRow()" />');
+}
